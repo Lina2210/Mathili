4b43fd853c246641d5dada3df8dc75d8102d7261
Tianle <tianleyin8888@gmail.com>
2023-10-19 19:48:14 +0200

    Changes in index and new file Win

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +9,9 @@
 <body>
     <h1>Quien Quiere Ser Millonario?</h1>
     <h4>Instrucciones:</h4>
+    <ul>
+        <li></li>
+    </ul>
     <p></p>
     <img src="public/chica-riendo-feliz-chaqueta-mezclilla-billetes-voladores-alrededor.jpg" style="max-width: 300px;">
     <a href="game.php">JUGAR</a>
---
+++ win.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VICTORIA</title>
+</head>
+<body>
+    <h1>!HAS RESPONDIDO CORRECTAMENTE TODAS LAS PREGUNTAS!</h1>
+    <img src="public/silueta-personas-felices.jpg">
+    <a href="index.php">INICIO</a>
+    <a href="">GUARDAR STATS</a>
+</body>
+</html>
