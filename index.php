222f2e49fbcadc77a7b34539b0a0ca966038a066
Tianle <tianleyin8888@gmail.com>
2023-10-19 19:05:19 +0200

    Index and photos

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quien Quiere Ser Millonario?</title>
+    <link rel="icon" href="public/dolar.png" type="image/png">
+</head>
+<body>
+    <h1>Quien Quiere Ser Millonario?</h1>
+    <h4>Instrucciones:</h4>
+    <p></p>
+    <img src="public/chica-riendo-feliz-chaqueta-mezclilla-billetes-voladores-alrededor.jpg" style="max-width: 300px;">
+    <button onclick="startQuestions()">JUGAR</button>
+</body>
+</html>
