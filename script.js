17c139a0ea949319f55398097eeda074a5693953
Tianle <tianleyin8888@gmail.com>
2023-10-30 19:11:35 +0100

    30/10

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function() {
 function showFirstQuestion(){
     document.getElementById('question1').style.display = 'block';
     document.getElementById('answers1').style.display = 'grid';
+    document.getElementById('introQuestion1').style.display = 'block';
 }
 function good(numberOfQuestion){
     document.getElementById('correct'+numberOfQuestion).style.display = 'block';
@@ -36,6 +37,7 @@ function playSound(element){
 function loseSound(){
     let element = elementSound("soundLose")
     return playSound(element)
+
 }
 
 function acertSound(){
@@ -45,6 +47,7 @@ function acertSound(){
 function showQuestion(numberOfQuestion){
     document.getElementById('question'+numberOfQuestion).style.display = 'block';
     document.getElementById('answers'+numberOfQuestion).style.display = 'grid';
+    document.getElementById('introQuestion'+numberOfQuestion).style.display = 'block';
     document.getElementById('question'+numberOfQuestion).scrollIntoView({behavior:'smooth'})
 }
 
@@ -65,4 +68,33 @@ function loseSound(){
     let element = elementSound("soundLose")
     playSound(element)
     
+}
+
+function parteAEjecutar() {
+    const answerBad = document.getElementsByClassName("answer-button");
+    let arrayAnswer = [];
+
+    for (let i = 0; i < answerBad.length; i++) {
+        const pointerEventsValue = window.getComputedStyle(answerBad[i]).clientHeight;
+        let clientHeight = answerBad[i].clientHeight;
+
+        if (pointerEventsValue !== "none" && clientHeight !== 0) {
+            arrayAnswer.push(i);
+        }
+    }
+
+    console.log(arrayAnswer); // Muestra el contenido del array una vez terminado el bucle
+
+    let deleteAnswer = arrayAnswer.slice(-3);
+    let deleteIndex = Math.floor(Math.random() * deleteAnswer.length);
+    deleteAnswer.splice(deleteIndex, 1);
+
+    deleteAnswer.forEach(function (elemento) {
+        answerBad[elemento].innerText = "";
+    });
+    }
+function clickFifty(){
+    boton = document.getElementById("fifty")
+    boton.style.backgroundColor= "#fff";
+    parteAEjecutar()
 }
